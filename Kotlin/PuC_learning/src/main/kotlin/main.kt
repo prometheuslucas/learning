64d0730c432590f70c21938fd69f54e07001cb1f
Lucas_Lohmann <prometeolucas@hotmail.com>
2021-05-02 15:28:15 +0200

    14.4.2021 1 hour FIRST

--- Kotlin/PuC_learning/src/main/kotlin/main.kt
+++ Kotlin/PuC_learning/src/main/kotlin/main.kt
@@ -1,3 +1,23 @@
+
+sealed class Expression{
+    data class Number(val number : Int) : Expression()
+    data class Addition(val summand_1: Expression,val summand_2: Expression) : Expression()
+}
+
+fun evaluate(expression: Expression): Int{
+    return when(expression){
+        is Expression.Number -> expression.number
+        is Expression.Addition -> evaluate(expression.summand_1) + evaluate(expression.summand_2)
+    }
+}
+
 fun main(args: Array<String>) {
-    println("FUCK YEAH!!!")
+    val addition = Expression.Addition(
+            Expression.Addition(
+                    Expression.Number(1),
+                    Expression.Number(9)
+            ),
+            Expression.Number(3)
+    )
+    println("$addition = ${evaluate(addition)}")
 }
